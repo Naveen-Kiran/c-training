754c48c62e7fceb37a583f17d7fd3d08f21659c0
Naveen kiran <naveenkiran.dabbikar@gmail.com>
2019-01-12 09:40:50 +0530

    assignement

---
+++ operators/assignment.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int i = 5, j = 6;
+	int k;
+	k = 3, 5;
+	i = (3, 5);
+	j = (3, 5, 9);
+	printf("Value of k is %d\n", k);
+	printf("Value of i is %d\n", i);
+	printf("Value of j is %d\n", j);
+}
