012b48bc93e1d4d284a3d2ff3c4f34656b68920f
Naveen kiran <naveenkiran.dabbikar@gmail.com>
2019-01-12 07:50:59 +0530

    Arithmetic operators

---
+++ operators/arithmetic.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int a, b;
+	a = 5;
+	b = 4;
+
+	printf("add of a + b is %d\n", a+b);
+	printf("subtract of a - b is %d\n", a-b);
+	printf("multiplication of a * b is %d\n", a*b);
+	printf("division of a / b is %d\n", a/b);
+	printf("remainder of a %% b is %d\n", a%b);
+	return 0;
+	
+
+}
