d05eee079040b1775eb2a98965223ae9f361659e
Naveen kiran <naveenkiran.dabbikar@gmail.com>
2019-01-12 09:45:22 +0530

    greater

---
+++ operators/greater.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	int a , b;
+	printf("Enter a \n");
+	scanf("%d", &a);
+	printf("Enter b:\n");
+	scanf("%d", &b);
+
+	if(a>b)
+          printf("a is greater\n");
+        else
+	{
+          printf("b is greater\n");
+	 printf("hello\n");
+	}
+}
